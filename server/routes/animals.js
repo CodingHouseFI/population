815dbc1c75167d522d30246e01ccdf19c5721bb5
Cade Nichols <c@codinghouse.co>
2016-08-23 12:28:11 -0700

    associating animals and people

---
+++ server/routes/animals.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const Animal = require('../models/animal');
+
+//  /api/animals
+
+router.route('/')
+  .get((req, res) => {
+    Animal.find({}, (err, animals) => {
+      res.status(err ? 400 : 200).send(err || animals);
+    })
+  })
+  .post((req, res) => {
+    Animal.create(req.body, (err, animal) => {
+      res.status(err ? 400 : 200).send(err || animal);
+    })
+  });
+
+
+router.put('/:animalId/addOwner/:ownerId', (req, res) => {
+  Animal.findById(req.params.animalId, (err, animal) => {
+    if(err || !animal) {
+      return res.status(400).send(err || 'Animal not found.');
+    }
+    
+    let ownerId = req.params.ownerId;
+
+    animal.owner = ownerId;
+
+    animal.save((err, savedAnimal) => {
+      res.status(err ? 400 : 200).send(err || savedAnimal);
+    });
+  });
+});
+
+module.exports = router;
